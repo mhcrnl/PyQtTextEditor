af463c830504e8a8b56e4e141ee2557cefa4f0ae
mhcrnl <mhcrnl@gmail.com>
2020-12-15 20:32:50 +0200

    marți 15 decembrie 2020, 20:32:50 +0200

--- text_editor.py
+++ text_editor.py
@@ -3,11 +3,14 @@
 # Run: python3 text_editor.py
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QMainWindow, QMessageBox
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QMainWindow, QMessageBox, QFileDialog
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QAction
+from PyQt5.QtCore import QDir
+from PyQt5.QtGui import *
 
 class text_editor(QtWidgets.QMainWindow):
     def __init__(self):
@@ -19,9 +22,9 @@ class text_editor(QtWidgets.QMainWindow):
         self.setWindowIcon(QtGui.QIcon("PyQtTextEditor/images/home1.png"))
         self.show()
         # Adding QTextEdit widget -------------------------
-        text = QTextEdit(self)
-        text.setTabStopWidth(20000)
-        self.setCentralWidget(text)
+        self.text = QTextEdit(self)
+        self.text.setTabStopWidth(20000)
+        self.setCentralWidget(self.text)
         # File menu New -------------------------------------
         new = QAction(QIcon("images/color.ico"), "New", self)
         new.setStatusTip("Creaza un nou document.")
@@ -29,7 +32,7 @@ class text_editor(QtWidgets.QMainWindow):
         # File menu Open ------------------------------------
         open = QAction(QIcon("images/color.ico"), "Open File", self)
         open.setStatusTip("Deschide un document.")
-        open.triggered.connect(self.Open)
+        open.triggered.connect(self.Open1)
         # File menu Save ---------------------------------------------
         save = QAction(QIcon("images/color.ico"), "Save", self)
         save.setToolTip("<h2>Salveaza un document.</h2>")
@@ -38,14 +41,32 @@ class text_editor(QtWidgets.QMainWindow):
         exit = QAction(QIcon("images/color.ico"), "Exit", self)
         exit.setStatusTip("Exit application.")
         exit.triggered.connect(self.Exit)
+        #Cut --------------------------------------------------------
+        cut = QAction(QIcon("images/color.ico"), "Cut", self)
+        cut.setStatusTip("Delete and copy text to clipboard.")
+        cut.triggered.connect(self.Cut)
+        # Copy ---------------------------------------------------
+        copy = QAction(QIcon("images/color.ico"), "Copy", self)
+        copy.setStatusTip("Copy text to clipboard.")
+        copy.triggered.connect(self.Copy)
 
         menubar = self.menuBar()
         file = menubar.addMenu("File")
+        edit = menubar.addMenu("Edit")
 
         file.addAction(new)
         file.addAction(open)
         file.addAction(save)
         file.addAction(exit)
+
+        edit.addAction(cut)
+        edit.addAction(copy)
+
+    def Copy(self):
+        self.text.copy()
+
+    def Cut(self):
+        self.text.cut()
 
     def Exit(self):
         f = QMessageBox.question(self, "PyQtTextEditor", "Doriti sa inchideti aplicatia?",
@@ -62,9 +83,22 @@ class text_editor(QtWidgets.QMainWindow):
         self.New = text_editor()
         self.New.show()
 
+    def Open1(self):
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.AnyFile)
+        dlg.setNameFilters(["Text files (*.txt)"])
+        #filenames = QString()
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            f = open (filenames[0], "r")
+            with f:
+                data = f.read()
+                self.text.setText(data)
+
+
     def Open(self):
         filename = QtWidgets.QFileDialog.getOpenFileName(self, "Open File",
-".", "(*.*)All Files")[0]
+"~/", "(*.txt *.md)All Files")[0]
         if filename == '':
             text_editor()
         else:
