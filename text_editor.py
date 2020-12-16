4604cfbdc65b2935191551f453b1fca493fd8b69
mhcrnl <mhcrnl@gmail.com>
2020-12-16 19:06:42 +0200

    miercuri 16 decembrie 2020, 19:06:42 +0200

--- text_editor.py
+++ text_editor.py
@@ -15,18 +15,29 @@ from PyQt5.QtGui import *
 class text_editor(QtWidgets.QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
+
+        self.prog_name = "PyQtTextEditor"
+        self.version = "0.01"
+        self.author = "mhcrnl@gmail.com"
+
         # Window ----------------------------------------
-        self.setWindowTitle("PyQtTextEditor")
+        self.setWindowTitle(self.prog_name)
         self.isMaximized()
         self.setFixedSize(850, 500)
         self.setWindowIcon(QtGui.QIcon("PyQtTextEditor/images/home1.png"))
+        self.statusBar()
         self.show()
+        # Adding PyQt ToolBar -------------------------------------------
+        self.toolbar = self.addToolBar("Exit")
+        #self.toolbar.addAction(exit)
+
         # Adding QTextEdit widget -------------------------
         self.text = QTextEdit(self)
         self.text.setTabStopWidth(20000)
         self.setCentralWidget(self.text)
         # File menu New -------------------------------------
-        new = QAction(QIcon("images/color.ico"), "New", self)
+        new = QAction(QIcon("icons/new.png"), "New", self)
+        new.setShortcut("Ctrl+N")
         new.setStatusTip("Creaza un nou document.")
         new.triggered.connect(self.New)
         # File menu Open ------------------------------------
@@ -35,7 +46,7 @@ class text_editor(QtWidgets.QMainWindow):
         open.triggered.connect(self.Open1)
         # File menu Save ---------------------------------------------
         save = QAction(QIcon("images/color.ico"), "Save", self)
-        save.setToolTip("<h2>Salveaza un document.</h2>")
+        save.setStatusTip("<h2>Salveaza un document.</h2>")
         save.triggered.connect(self.Save)
         # File menu Exit ---------------------------------------------
         exit = QAction(QIcon("images/color.ico"), "Exit", self)
@@ -49,10 +60,27 @@ class text_editor(QtWidgets.QMainWindow):
         copy = QAction(QIcon("images/color.ico"), "Copy", self)
         copy.setStatusTip("Copy text to clipboard.")
         copy.triggered.connect(self.Copy)
+        # Paste --------------------------------------------------
+        paste = QAction(QIcon("images/color.ico"), "Paste", self)
+        paste.setStatusTip("Paste text from clipboard")
+        paste.triggered.connect(self.Paste)
+        # Undo ---------------------------------------------------
+        undo = QAction(QIcon("images/color.ico"), "Undo", self)
+        undo.setStatusTip("Undo last action.")
+        undo.triggered.connect(self.Undo)
+        # Redo ---------------------------------------------------
+        redo = QAction(QIcon("images/color.ico"), "Redo", self)
+        redo.setStatusTip("Redo last undone thing.")
+        redo.triggered.connect(self.Redo)
+        # Info --------------------------------------------------
+        info = QAction(QIcon("images/color.ico"), "Info", self)
+        info.setStatusTip("Information by developer.")
+        info.triggered.connect(self.Info)
 
         menubar = self.menuBar()
         file = menubar.addMenu("File")
         edit = menubar.addMenu("Edit")
+        help = menubar.addMenu("Help")
 
         file.addAction(new)
         file.addAction(open)
@@ -61,6 +89,25 @@ class text_editor(QtWidgets.QMainWindow):
 
         edit.addAction(cut)
         edit.addAction(copy)
+        edit.addAction(paste)
+        edit.addAction(undo)
+        edit.addAction(redo)
+
+        help.addAction(info)
+
+        self.toolbar.addAction(exit)
+
+    def Info(self):
+        f = QMessageBox.about(self, "About", "PyQtTextEditor\n\n Version 0.01 \n\n A simple text editor in python3 and PyQt5")
+
+    def Redo(self):
+        self.text.redo()
+
+    def Undo(self):
+        self.text.undo()
+
+    def Paste(self):
+        self.text.paste()
 
     def Copy(self):
         self.text.copy()
